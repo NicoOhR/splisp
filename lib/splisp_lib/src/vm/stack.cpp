22fe11afb913ad60abcf9db48eb9eb9dd205cea8
Nico OR <nohayonr@gmail.com>
2025-12-29 15:42:59 -0600

    finished all instruction implementations

--- lib/splisp_lib/src/vm/stack.cpp
+++ lib/splisp_lib/src/vm/stack.cpp
@@ -7,22 +7,37 @@
 #include <stdexcept>
 #include <vm/stack.hpp>
 
-Stack::Stack(std::vector<ISA::Instruction> program)
-    : program_mem(std::move(program)) {};
+Stack::Stack(std::vector<ISA::Instruction> program, std::vector<uint8_t> data) {
+  std::vector<uint8_t> program_bytes;
+  for (auto instr : program) {
+    auto tmp = instr.to_bytes();
+    program_bytes.insert(program_bytes.end(), tmp.begin(), tmp.end());
+  }
+  this->program_mem.insert(this->program_mem.end(), program_bytes.begin(),
+                           program_bytes.end());
+  this->program_mem.insert(this->program_mem.end(), data.begin(), data.end());
+};
+
+uint64_t read_operand(const std::vector<uint8_t> &mem, size_t pc) {
+  uint64_t value = 0;
+  for (size_t i = 0; i < 8; ++i) {
+    value |= static_cast<uint64_t>(mem[pc + 1 + i]) << (8 * i);
+  }
+  return value;
+}
 
 void Stack::advanceProgram() {
   MachineState state = runInstruction();
   if (state == OKAY) {
-    this->pc += 1;
+    this->pc += 9;
   } else {
     throw std::exception();
   }
 };
 
 MachineState Stack::runInstruction() {
-  ISA::Instruction curr = this->program_mem[this->pc];
-  auto op_idx = static_cast<size_t>(curr.op);
-  const auto spec = ISA::spec_list[op_idx];
+  uint8_t curr = this->program_mem[this->pc];
+  const auto spec = ISA::spec_list[curr];
   switch (spec.operation) {
   case (ISA::OperationKind::ARITHMETIC): {
     return this->handleArithmetic(curr, spec);
@@ -37,18 +52,19 @@ MachineState Stack::runInstruction() {
     return this->handleLogic(curr, spec);
   }
   default:
-    break;
+    return MachineState::INVALID_OP;
   }
 };
 
-MachineState Stack::handleArithmetic(ISA::Instruction instr, ISA::Spec spec) {
-  switch (instr.op) {
+MachineState Stack::handleArithmetic(uint8_t op, ISA::Spec spec) {
+  switch (static_cast<ISA::Operation>(op)) {
   case (ISA::Operation::ADD): {
     auto a = data_stack.top();
     data_stack.pop();
     auto b = data_stack.top();
     data_stack.pop();
     data_stack.push(a + b);
+    break;
   }
   case (ISA::Operation::SUB): {
     auto a = data_stack.top();
@@ -56,6 +72,7 @@ MachineState Stack::handleArithmetic(ISA::Instruction instr, ISA::Spec spec) {
     auto b = data_stack.top();
     data_stack.pop();
     data_stack.push(a - b);
+    break;
   }
   case (ISA::Operation::MUL): {
     auto a = data_stack.top();
@@ -63,6 +80,7 @@ MachineState Stack::handleArithmetic(ISA::Instruction instr, ISA::Spec spec) {
     auto b = data_stack.top();
     data_stack.pop();
     data_stack.push(a * b);
+    break;
   }
   case (ISA::Operation::DIV): {
     auto a = data_stack.top();
@@ -70,6 +88,7 @@ MachineState Stack::handleArithmetic(ISA::Instruction instr, ISA::Spec spec) {
     auto b = data_stack.top();
     data_stack.pop();
     data_stack.push(a / b);
+    break;
   }
   case (ISA::Operation::MOD): {
     auto a = data_stack.top();
@@ -77,16 +96,19 @@ MachineState Stack::handleArithmetic(ISA::Instruction instr, ISA::Spec spec) {
     auto b = data_stack.top();
     data_stack.pop();
     data_stack.push(a % b);
+    break;
   }
   case (ISA::Operation::INC): {
     auto a = data_stack.top();
     data_stack.pop();
     data_stack.push(a + 1);
+    break;
   }
   case (ISA::Operation::DEC): {
     auto a = data_stack.top();
     data_stack.pop();
     data_stack.push(a - 1);
+    break;
   }
   case (ISA::Operation::MAX): {
     auto a = data_stack.top();
@@ -94,6 +116,7 @@ MachineState Stack::handleArithmetic(ISA::Instruction instr, ISA::Spec spec) {
     auto b = data_stack.top();
     data_stack.pop();
     data_stack.push(std::max(a, b));
+    break;
   }
   case (ISA::Operation::MIN): {
     auto a = data_stack.top();
@@ -101,6 +124,7 @@ MachineState Stack::handleArithmetic(ISA::Instruction instr, ISA::Spec spec) {
     auto b = data_stack.top();
     data_stack.pop();
     data_stack.push(std::min(a, b));
+    break;
   }
   default:
     throw std::invalid_argument(
@@ -108,14 +132,15 @@ MachineState Stack::handleArithmetic(ISA::Instruction instr, ISA::Spec spec) {
   }
   return MachineState::OKAY;
 };
-MachineState Stack::handleLogic(ISA::Instruction instr, ISA::Spec spec) {
-  switch (instr.op) {
+MachineState Stack::handleLogic(uint8_t op, ISA::Spec spec) {
+  switch (static_cast<ISA::Operation>(op)) {
   case (ISA::Operation::LT): {
     auto a = data_stack.top();
     data_stack.pop();
     auto b = data_stack.top();
     data_stack.pop();
     data_stack.push(a < b ? 1 : 0);
+    break;
   }
   case (ISA::Operation::LE): {
     auto a = data_stack.top();
@@ -123,6 +148,7 @@ MachineState Stack::handleLogic(ISA::Instruction instr, ISA::Spec spec) {
     auto b = data_stack.top();
     data_stack.pop();
     data_stack.push(a <= b ? 1 : 0);
+    break;
   }
   case (ISA::Operation::EQ): {
     auto a = data_stack.top();
@@ -130,6 +156,7 @@ MachineState Stack::handleLogic(ISA::Instruction instr, ISA::Spec spec) {
     auto b = data_stack.top();
     data_stack.pop();
     data_stack.push(a == b ? 1 : 0);
+    break;
   }
   case (ISA::Operation::GE): {
     auto a = data_stack.top();
@@ -137,6 +164,7 @@ MachineState Stack::handleLogic(ISA::Instruction instr, ISA::Spec spec) {
     auto b = data_stack.top();
     data_stack.pop();
     data_stack.push(a > b ? 1 : 0);
+    break;
   }
   case (ISA::Operation::GT): {
     auto a = data_stack.top();
@@ -144,6 +172,7 @@ MachineState Stack::handleLogic(ISA::Instruction instr, ISA::Spec spec) {
     auto b = data_stack.top();
     data_stack.pop();
     data_stack.push(a >= b ? 1 : 0);
+    break;
   }
   default:
     throw std::invalid_argument(
@@ -151,18 +180,22 @@ MachineState Stack::handleLogic(ISA::Instruction instr, ISA::Spec spec) {
   }
   return MachineState::OKAY;
 };
-MachineState Stack::handleTransfer(ISA::Instruction instr, ISA::Spec spec) {
-  switch (instr.op) {
+MachineState Stack::handleTransfer(uint8_t op, ISA::Spec spec) {
+  const uint64_t operand = read_operand(this->program_mem, this->pc);
+  switch (static_cast<ISA::Operation>(op)) {
   case (ISA::Operation::DROP): {
     data_stack.pop();
+    break;
   }
   case (ISA::Operation::DUP): {
     data_stack.push(data_stack.top());
+    break;
   }
   case (ISA::Operation::NDUP): {
-    for (auto i = 1; i < instr.operand.value(); i++) {
+    for (auto i = 1; i < operand; i++) {
       data_stack.push(data_stack.top());
     }
+    break;
   }
   case (ISA::Operation::SWAP): {
     auto a = data_stack.top();
@@ -171,6 +204,7 @@ MachineState Stack::handleTransfer(ISA::Instruction instr, ISA::Spec spec) {
     data_stack.pop();
     data_stack.push(b);
     data_stack.push(a);
+    break;
   }
   case (ISA::Operation::ROT): {
     auto a = data_stack.top();
@@ -182,12 +216,13 @@ MachineState Stack::handleTransfer(ISA::Instruction instr, ISA::Spec spec) {
     data_stack.push(a);
     data_stack.push(c);
     data_stack.push(b);
+    break;
   }
   case (ISA::Operation::NROT): {
     // stupid implementations are stupid
     auto tmp = std::list<uint64_t>();
     // append from stack in decending order
-    for (auto i = 0; i < instr.operand.value(); i++) {
+    for (auto i = 0; i < operand; i++) {
       tmp.push_front(data_stack.top());
       data_stack.pop();
     }
@@ -200,6 +235,7 @@ MachineState Stack::handleTransfer(ISA::Instruction instr, ISA::Spec spec) {
       data_stack.push(tmp.back());
       tmp.pop_back();
     }
+    break;
   }
   case (ISA::Operation::TUCK): {
     auto a = data_stack.top();
@@ -211,11 +247,12 @@ MachineState Stack::handleTransfer(ISA::Instruction instr, ISA::Spec spec) {
     data_stack.push(b);
     data_stack.push(a);
     data_stack.push(c);
+    break;
   }
   case (ISA::Operation::NTUCK): {
     auto tmp = std::list<uint64_t>();
     // append from stack in decending order
-    for (auto i = 0; i < instr.operand.value(); i++) {
+    for (auto i = 0; i < operand; i++) {
       tmp.push_front(data_stack.top());
       data_stack.pop();
     }
@@ -226,22 +263,67 @@ MachineState Stack::handleTransfer(ISA::Instruction instr, ISA::Spec spec) {
     }
     // insert the last element (n) into the top of the N operations
     data_stack.push(tmp.back());
+    break;
   }
   case (ISA::Operation::SIZE): {
     data_stack.push(data_stack.size());
+    break;
   }
   case (ISA::Operation::NRND): {
     throw std::invalid_argument("not yet implemented");
+    break;
   }
   case (ISA::Operation::PUSH): {
-    data_stack.push(instr.operand.value());
+    data_stack.push(operand);
+    break;
   }
   case (ISA::Operation::FETCH): {
-    auto add = data_stack.top();
+    auto add = static_cast<size_t>(data_stack.top());
     data_stack.pop();
-    data_stack.push(
-        static_cast<uint64_t>(this->program_mem[static_cast<size_t>(add)]));
+    uint8_t val = this->program_mem[add];
+    data_stack.push(val);
+    break;
   }
+  default:
+    throw std::invalid_argument("non-control operation handed to");
   }
-};
-MachineState Stack::handleControl(ISA::Instruction, ISA::Spec){};
+  return MachineState::OKAY;
+}
+MachineState Stack::handleControl(uint8_t op, ISA::Spec) {
+  switch (static_cast<ISA::Operation>(op)) {
+  case (ISA::Operation::CALL): {
+    this->return_stack.push(this->pc);
+    auto dest = this->data_stack.top();
+    data_stack.pop();
+    this->pc = dest;
+  }
+  case (ISA::Operation::RET): {
+    auto dest = this->return_stack.top();
+    this->return_stack.pop();
+    this->pc = dest;
+  }
+  case (ISA::Operation::JMP): {
+    auto dest = this->data_stack.top();
+    this->data_stack.pop();
+    this->pc = dest;
+  }
+  case (ISA::Operation::CJMP): {
+    auto a = this->data_stack.top();
+    this->data_stack.pop();
+    auto b = this->data_stack.top();
+    this->data_stack.pop();
+    if (a != 0) {
+      this->pc = b;
+    }
+  }
+  case (ISA::Operation::WAIT): {
+    return MachineState::OKAY;
+  }
+  case (ISA::Operation::HALT): {
+    return MachineState::HALT;
+  }
+  default:
+    throw std::invalid_argument(
+        "Non-control operation dispatched to control handler");
+  }
+}
